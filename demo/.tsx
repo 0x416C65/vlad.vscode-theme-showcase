541245eb017258992358efa50716c1b7e85d3696
└ Disco Local (C:) <0x416c+github@gmail.com>
2023-06-25 01:31:06 -0300

    Add demo/.tsx

---
+++ demo/.tsx
@@ -0,0 +1,35 @@
+// @ts-nocheck
+import { useState } from 'react';
+
+type ThemeProps = {
+  name: '#Vlad';
+  twitterHandle: '0x416C65';
+  githubHandle: '0x416C65';
+  initialDownloads: 100_000_000;
+};
+
+const ThemeComponent = ({
+  twitterHandle,
+  githubHandle,
+  ...theme
+}: ThemeProps) => {
+  const [downloadsCount, setDownloadsCount] = useState(theme.initialDownloads);
+  const incrementDownloadsCount = () =>
+    setDownloadsCount(prevCount => prevCount + 1);
+
+  return (
+    <div>
+      <h1>🎨 Welcome to {theme.name} theme!</h1>
+      <p>Total Downloads: {downloadsCount}</p>
+      <button onClick={incrementDownloadsCount}>Install Theme</button>
+      <p>
+        Follow me on
+        <a href={`https://twitter.com/${twitterHandle}`}>Twitter</a> and
+        <a href={`https://github.com/${githubHandle}`}>GitHub</a>, and don't
+        forget to download the theme.
+      </p>
+    </div>
+  );
+};
+
+export default ThemeComponent;
